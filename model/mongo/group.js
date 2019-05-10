9ef071e5e9a348229bfdc77ceac2bcec0f466782
antoinehage <me@antoinehage.com>
2019-05-10 16:24:18 -0400

    added groups indexing

--- model/mongo/group.js
+++ model/mongo/group.js
@@ -4,11 +4,21 @@ const userColName = "users";
 const core = require("soajs");
 const Mongo = core.mongo;
 
+let indexing = {};
+
 function Group(soajs) {
     let __self = this;
     __self.soajs = soajs;
     if (!__self.mongoCore) {
-        __self.mongoCore = new Mongo(__self.soajs.registry.coreDB.provision);
+        __self.mongoCore = new Mongo(soajs.meta.tenantDB(soajs.registry.tenantMetaDB, soajs.config.serviceName, soajs.tenant.code));
+        if (indexing && soajs && soajs.tenant && soajs.tenant.id && !indexing[soajs.tenant.id]) {
+            indexing[soajs.tenant.id] = true;
+
+            __self.mongoCore.createIndex(colName, {'code': 1}, {unique: true}, function (err, result) {});
+            __self.mongoCore.createIndex(colName, {'tenant.id': 1}, {unique: true}, function (err, result) {});
+
+            __self.soajs.log.debug("Indexes for "+ soajs.tenant.id +" Updated!");
+        }
     }
 }
 
