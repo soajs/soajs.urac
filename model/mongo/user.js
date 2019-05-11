3a3855750450d1b6593c2a70c50ea39a9f08e4c2
antoinehage <me@antoinehage.com>
2019-05-10 21:00:11 -0400

    added getUser

--- model/mongo/user.js
+++ model/mongo/user.js
@@ -14,12 +14,36 @@ function User(soajs) {
             indexing[soajs.tenant.id] = true;
 
             //needed by deleteGroup @ groups to pull group from all users once deleted
-            __self.mongoCore.createIndex(colName, {'tenant.id': 1}, {}, function (err, result) {});
+            __self.mongoCore.createIndex(colName, {'tenant.id': 1}, {}, function (err, result) {
+            });
 
-            __self.soajs.log.debug("Indexes for "+ soajs.tenant.id +" Updated!");
+            __self.soajs.log.debug("Indexes for " + soajs.tenant.id + " Updated!");
         }
     }
 }
+
+/**
+ * To get a user
+ *
+ * inputmaskData should have
+ *      required (id)
+ *
+ * @param cb
+ */
+Group.prototype.getUser = function (cb) {
+    let __self = this;
+    let condition = {};
+    if (__self.soajs.inputmaskData.id) {
+        condition = {'_id': __self.soajs.inputmaskData.id};
+
+        __self.mongoCore.findOne(colName, condition, {socialId: 0, password: 0}, null, (err, record) => {
+            if (err) {
+                return cb(err);
+            }
+            return cb(null, record);
+        });
+    }
+};
 
 User.prototype.closeConnection = function (cb) {
     let __self = this;
