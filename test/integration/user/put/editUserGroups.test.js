ad8bb1d99b9b9b3b196c7f68b3325a8dfc737f7f
antoinehage <me@antoinehage.com>
2020-01-30 12:54:24 -0500

    bumped version to 3.0.23

--- test/integration/user/put/editUserGroups.test.js
+++ test/integration/user/put/editUserGroups.test.js
@@ -6,7 +6,7 @@ let validator = new core.validator.Validator();
 let getUsersSchema = require("../schemas/getUsers");
 let editUserSchema = require("../schemas/editUserGroups");
 
-let clientKey = 'e267a49b84bfa1e95dffe1efd45e443f36d7dced1dc97e8c46ce1965bac78faaa0b6fe18d50efa5a9782838841cba9659fac52a77f8fa0a69eb0188eef4038c49ee17f191c1d280fde4d34580cc3e6d00a05a7c58b07a504f0302915bbe58c18';
+//let clientKey = 'e267a49b84bfa1e95dffe1efd45e443f36d7dced1dc97e8c46ce1965bac78faaa0b6fe18d50efa5a9782838841cba9659fac52a77f8fa0a69eb0188eef4038c49ee17f191c1d280fde4d34580cc3e6d00a05a7c58b07a504f0302915bbe58c18';
 
 describe("Testing edit User Groups API", () => {
 
