9f3c6454aa689522b4e0e723cd523738d6d83688
antoinehage <me@antoinehage.com>
2021-11-24 14:19:15 -0500

    added log

--- bl/lib/joinCode.js
+++ bl/lib/joinCode.js
@@ -36,6 +36,7 @@ let local = (soajs, inputmaskData, options, cb) => {
     inputmaskData.keepToken = true;
     inputmaskData.doNotSendEmail = (inputmaskData.confirmation === "phone") || emailCode;
     console.log(inputmaskData);
+    console.log(emailCode);
     bl.join(soajs, inputmaskData, options, (error, response) => {
         if (error) {
             bl.user.mt.closeModel(modelObj);
