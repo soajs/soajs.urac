e0459f257d30d07a5ed686fbcab0e9476f06f83e
antoinehage <me@antoinehage.com>
2022-01-31 12:55:24 -0500

    enhanced how we set status

--- _index.js
+++ _index.js
@@ -74,7 +74,7 @@ function run(serviceStartCb) {
                 });
             });
             service.get("/user", function (req, res) {
-                req.soajs.inputmaskData.keep = !!inputmaskData.config;
+                req.soajs.inputmaskData.keep = !!req.soajs.inputmaskData.config;
                 bl.user.getUserByUsername(req.soajs, req.soajs.inputmaskData, null, (error, data) => {
                     return res.json(req.soajs.buildResponse(error, data));
                 });
